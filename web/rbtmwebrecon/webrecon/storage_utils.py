496efb3f9bd6385660d070ab6826092baf1fa1d7
Alexey Buzmakov <buzmakov@gmail.com>
2018-10-09 15:23:41 +0300

    first commit

---
+++ web/rbtmwebrecon/webrecon/storage_utils.py
@@ -0,0 +1,54 @@
+import json
+import requests
+import tomo_queue
+import glob
+import os
+
+
+STORAGE_SERVER = "http://rbtmstorage_server_1:5006/"
+
+def get_reconstructed_files_list(experiment_id):
+    app_root = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
+    app_tomo_data= os.path.join(app_root, 'static', 'tomo_data', experiment_id)
+    res = {}
+    if not os.path.isdir(app_tomo_data):
+        return res
+    
+    if os.path.exists(os.path.join(app_tomo_data,'tomo.html')):
+        res['tomo_preview'] =  '/static/tomo_data/' + experiment_id +'/tomo.html'
+    
+    if os.path.exists(os.path.join(app_tomo_data,'tomo.hx')):
+        res['amira_hx'] =  '/static/tomo_data/' + experiment_id +'/tomo.hx'
+
+    if os.path.exists(os.path.join(app_tomo_data,'amira.raw')):
+        res['amira_raw'] =  '/static/tomo_data/' + experiment_id +'/amira.raw'
+    
+    if os.path.exists(os.path.join(app_tomo_data,'tomo_rec.h5')):
+        res['tomo_rec'] =  '/static/tomo_data/' + experiment_id +'/tomo_rec.h5'
+
+    tomo_reports = glob.glob(os.path.join(app_tomo_data,'reconstructor-v*.html'))
+
+    if len(tomo_reports)>0:
+       res['tomo_reports'] = [tr[len(app_root):] for tr in tomo_reports]
+
+    return res
+
+
+def get_tomoobject_info(experiment_id):
+    exp_info = json.dumps(({"_id": experiment_id}))
+    experiment = requests.post(STORAGE_SERVER + 'storage/experiments/get',
+                               exp_info, timeout=1000)
+    experiment_info = json.loads(experiment.content)[0]
+    tomo_status = tomo_queue.get_object_status(experiment_id)
+    experiment_info['tomo_status'] = tomo_status
+    experiment_info['files'] = get_reconstructed_files_list(experiment_id)
+    return  experiment_info
+
+def get_tomoobjects_list():
+    exp_info = json.dumps({'finished': True})
+    # print(exp_info)
+    experiment = requests.post(STORAGE_SERVER + 'storage/experiments/get',
+                               exp_info, timeout=1000)
+    experiment_info = json.loads(experiment.content)
+    ids = [x['_id'] for x in experiment_info]
+    return  ids
---
+++ web/rbtmwebrecon/webrecon/web_recon.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, jsonify, request, redirect
+from flask_restful import Resource, Api
+import pprint
+import tomo_queue
+
+import storage_utils
+
+app = Flask(__name__)
+api = Api(app)
+
+class TomoObjects(Resource):
+    def get(self):
+        return storage_utils.get_tomoobjects_list()
+
+api.add_resource(TomoObjects, '/tomo_objects')
+
+class TomoObject(Resource):
+    def get(self,to_id):
+        return storage_utils.get_tomoobject_info(to_id)
+
+api.add_resource(TomoObject,'/tomo_object/<to_id>')
+
+@app.route('/')
+@app.route('/view/tomo_objects')
+def view_tomo_objects():
+    to_ids=TomoObjects()
+    to = TomoObject()
+    tomo_objects = [to.get(to_id) for to_id in to_ids.get()]
+    tomo_objects.sort(key=lambda x:x['timestamp'], reverse=True)
+    return render_template('tomo_objects.html',
+                           tomo_objects=tomo_objects)
+
+
+@app.route('/view/tomo_object/<to_id>')
+def view_tomo_object(to_id):
+    to=TomoObject()
+    tomo_object = to.get(to_id)
+    return render_template('tomo_object.html',
+                       tomo_object_str=pprint.pformat(tomo_object),
+                       tomo_object=tomo_object)
+
+@app.route('/reconstruct/<to_id>')
+def reconstruct(to_id):
+    tomo_queue.put_object_rec_queue(to_id)
+    return redirect('/view/tomo_object/'+to_id)
+
+@app.route('/reset/<to_id>')
+def reset(to_id):
+    tomo_queue.set_object_status(to_id, 'canceled')
+    return redirect('/view/tomo_object/'+to_id)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='10.0.7.153', port=5550)
