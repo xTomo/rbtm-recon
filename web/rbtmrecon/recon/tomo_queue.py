338c8ed87838ac702a60182f9d7de7f58297d98c
Alexey Buzmakov <buzmakov@gmail.com>
2026-04-27 23:56:35 +0300

    add logging

--- web/rbtmrecon/recon/tomo_queue.py
+++ web/rbtmrecon/recon/tomo_queue.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 from pymongo import MongoClient, DESCENDING
@@ -27,6 +28,7 @@ def get_object(obj_id):
 
 
 def set_object_status(obj_id, status):
+    logging.info('[QUEUE] set_object_status: {} -> {}'.format(obj_id, status))
     to.insert_one({'obj_id': obj_id,
                'status': status,
                'date': datetime.now()}
@@ -42,14 +44,25 @@ def get_object_status(obj_id):
 
 
 def get_rec_queue_next_obj():
-    for obj in to.find({'status': 'waiting'}):
+    waiting_all = list(to.find({'status': 'waiting'}))
+    logging.info('[QUEUE] get_rec_queue_next_obj: всего waiting записей в БД: {}'.format(len(waiting_all)))
+    for obj in waiting_all:
         if 'action' not in obj:
+            logging.info('[QUEUE] пропускаем запись без action: {}'.format(obj.get('_id')))
             continue
         # Получаем последнюю запись для этого obj_id
         latest_obj = get_object(obj['obj_id'])
+        latest_status = latest_obj['status'] if latest_obj else 'None'
+        latest_id = latest_obj['_id'] if latest_obj else 'None'
+        logging.info('[QUEUE] obj_id={} waiting_id={} latest_id={} latest_status={} match={}'.format(
+            obj['obj_id'], obj['_id'], latest_id, latest_status,
+            latest_obj and latest_obj['_id'] == obj['_id']
+        ))
         # Проверяем, что текущая запись является последней И имеет статус 'waiting'
         if latest_obj and latest_obj['_id'] == obj['_id']:
+            logging.info('[QUEUE] -> взяли задание: obj_id={} action={}'.format(obj['obj_id'], obj.get('action')))
             return obj
+    logging.info('[QUEUE] -> очередь пуста')
     return None
 
 
